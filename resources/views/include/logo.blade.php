c069ed0c1e4445417b7d9a58621cec24d63ef7ca
MohamadmHaikal <m.haikal@diriw.com>
2022-03-03 14:18:31 +0200

    first commit

---
+++ resources/views/include/logo.blade.php
@@ -0,0 +1,7 @@
+<div class="tl-logo-area d-none d-md-block">
+    <div class="d-flex flex-row align-center justify-content-center logo-area">
+        <a href="{{ url('/') }}" class="nav-link pr-0 pl-0">
+            <img src="{{ url('assets/img/logo.png') }}" class="navbar-logo" alt="logo">
+        </a>
+    </div>
+</div>
